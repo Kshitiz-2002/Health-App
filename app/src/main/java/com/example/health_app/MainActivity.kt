80dbcc845f4961a2ce446201eca085393d434c0d
Kshitiz-2002 <kshitizpandey2002@gmail.com>
2023-09-28 16:07:31 +0530

    Created homepage activity

--- app/src/main/java/com/example/health_app/MainActivity.kt
+++ app/src/main/java/com/example/health_app/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.health_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.startuppage)
+        val getStarted = findViewById<Button>(R.id.submitBox)
+
+        getStarted.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this, homepage::class.java)
+            startActivity(intent)
+        })
     }
 }
