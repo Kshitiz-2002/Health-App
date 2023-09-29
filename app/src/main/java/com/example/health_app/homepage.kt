ad13e95c827625a894be4611638b5ebfe7f244e5
Kshitiz-2002 <kshitizpandey2002@gmail.com>
2023-09-29 21:03:56 +0530

    Created searchbarpage

--- app/src/main/java/com/example/health_app/homepage.kt
+++ app/src/main/java/com/example/health_app/homepage.kt
@@ -1,12 +1,43 @@
 package com.example.health_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class homepage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_homepage)
+
+        val search = findViewById<View>(R.id.searchbar)
+
+        search.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this, searchbarflow::class.java)
+            startActivity(intent)
+        })
+
+        val bookAppointment = findViewById<View>(R.id.bookappointment)
+        bookAppointment.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this, bookAppointment::class.java)
+            startActivity(intent)
+        })
+//
+//        val bookAppointmentIcon = findViewById<View>(R.id.bookAppointmentIcon)
+//        bookAppointmentIcon.setOnClickListener(View.OnClickListener {
+//            val intent = Intent(this, bookAppointment::class.java)
+//            startActivity(intent)
+//        })
+//        val bookAppointmentText1 = findViewById<View>(R.id.book_an_app)
+//        bookAppointmentText1.setOnClickListener(View.OnClickListener {
+//            val intent = Intent(this, bookAppointment::class.java)
+//            startActivity(intent)
+//        })
+//        val bookAppointmentText2 = findViewById<View>(R.id.nursing_fac)
+//        bookAppointmentText2.setOnClickListener(View.OnClickListener {
+//            val intent = Intent(this, bookAppointment::class.java)
+//            startActivity(intent)
+//        })
 
     }
 }
